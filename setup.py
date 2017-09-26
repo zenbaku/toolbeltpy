50d4776a3545b7717f014b11b46e4a35aaa046f2
Jonathan Urzua <zenbaku@gmail.com>
2017-09-25 23:56:44 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='toolbeltpy',
+    version='0.1',
+    description='toolbelt.py',
+    url='http://github.com/zenbaku/toolbeltpy',
+    author='Jonathan Urzúa',
+    author_email='zenbaku@gmail.com',
+    license='MIT',
+    packages=['toolbeltpy'],
+    zip_safe=False
+)
